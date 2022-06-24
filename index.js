8d65c1d4c0f6a18996acaa4847de766a3491d8f0
Dylan <dylan.vonwald@gmail.com>
2022-06-23 23:09:19 -0600

    all inquirer prompts written out, need to add db queries now

--- index.js
+++ index.js
@@ -12,41 +12,108 @@ const db = mysql.createConnection(
   console.log(`Connected to the employees_db`)
 );
 
-inquirer.prompt([
-  {
-    type: "list",
-    name: "MainMenu",
-    message: "What would you like to do?",
-    choices: [
-      "View All Employees",
-      "Add Employee",
-      "Update Employee Role",
-      "View All Roles",
-      "Add Role",
-      "View All Departments",
-      "Add Department",
-      "Quit",
-    ],
-  },
-]);
+function mainMenu() {
+  inquirer.prompt([
+    {
+      type: "list",
+      name: "MainMenu",
+      message: "What would you like to do?",
+      choices: [
+        "View All Employees", //Select all employees and details from database
+        "Add Employee", // trigger addEmployee function
+        "Update Employee Role", //
+        "View All Roles", // Select all roles from database
+        "Add Role", // insert new role into role table in database
+        "View All Departments", // select all departments from database
+        "Add Department", // insert new department into department table in db
+        "Quit", // exits out of the application
+      ],
+    },
+  ]);
+}
 
-inquirer.prompt([
-  {
-    type: "input",
-    name: "AddEmpFirstName",
-    message: "What is the employee's first name?",
-  },
-  {
-    type: "input",
-    name: "AddEmpLastName",
-    message: "What is the employee's last name?",
-  },
-  {
-    type: "input",
-    name: "AddEmpLastName",
-    message: "What is the employee's last name?",
-  },
-]);
+function viewAllEmp() {
+    db.query(`SELECT * FROM employee`)
+};
+
+function viewAllRoles() {
+    db.query(`SELECT * FROM role`)
+}
+
+function viewAllDept() {
+    db.query(`SELECT * FROM department`)
+}
+
+function addRole() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'addRoleName',
+            message: "What is the name of the role?"
+        },
+        {
+            type: 'input',
+            name: 'addRoleSalary',
+            message: "What is the salary of the role?"
+        },
+        {
+            type: 'list',
+            name: 'addRoleDept',
+            message: "What is the department for this role?"
+            choices: []
+        },
+    ])
+}
+
+
+function addEmployee() {
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "AddEmpFirstName",
+      message: "What is the employee's first name?",
+    },
+    {
+      type: "input",
+      name: "AddEmpLastName",
+      message: "What is the employee's last name?",
+    },
+    {
+      type: "list",
+      name: "AddEmpRole",
+      message: "What is the employee's role?",
+      choices: [],
+    },
+    {
+      type: "list",
+      name: "AddEmpMgr",
+      message: "Who is this employee's manager?",
+      choices: [],
+    },
+  ]);
+}
+
+function updateEmpRole() {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'UpdateEmpRole',
+            message: 'Which role do you want to assign the selected employee?',
+            choices: [],
+        }
+    ])
+}
+
+function addDept() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'addDeptName',
+            message: "What is the name of the department?"
+        }
+    ])
+}
+
 
 // No Front end
 // No Sequelize
